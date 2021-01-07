9283bb7705a25a9d243e18ca4b7846fd79e9fc5d
Julio Orellana <julioa.orellana@gmail.com>
2021-01-06 22:15:25 -0300

    Clase: 1

---
+++ clase1/clase1.js
@@ -0,0 +1,20 @@
+var variable_string = "soy un texto"
+var variable_number = -1 // number
+var variable_float = 0.3 // number
+var variable_boolean = false
+var variable_null = null
+var variable_undefined = undefined
+
+
+// Lo que hace el Engine
+// var variable_string = "soy un texto"
+
+// var variable_string = new String("soy un texto")
+// var variable_number = new Number(-1)
+
+// Prototipos JS
+String
+Number
+Boolean
+
+var aaa = "La Magia de Javascript"
